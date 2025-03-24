7efb08f94140d943d39f9e144328b656720f7b75
Piotr Miernik <pkmiernik@gmail.com>
2025-03-24 08:44:42 +0100

    added reports remote

--- apps/shell/module-federation.config.ts
+++ apps/shell/module-federation.config.ts
@@ -14,7 +14,7 @@ const config: ModuleFederationConfig = {
    * declare module 'my-external-remote';
    *
    */
-  remotes: ['mainDashboard'],
+  remotes: ['mainDashboard', 'reports'],
 };
 
 /**
--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'reports',
+    loadChildren: () => import('reports/Routes').then((m) => m!.remoteRoutes),
+  },
+  {
     path: 'main-dashboard',
     loadChildren: () =>
       import('mainDashboard/Routes').then((m) => m!.remoteRoutes),
